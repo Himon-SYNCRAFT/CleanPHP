f37a6c5a0fbd8e0559c1b1a38190f6b07459aa1a
Daniel Zawlocki <d.zawlocki@eltrox.pl>
2017-09-15 13:13:04 +0200

    Initial commit. Basic domain entities

---
+++ src/Domain/Entity/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CleanPhp\Invoicer\Domain\Entity;
+
+
+class Customer extends AbstractEntity {
+	protected $name;
+	protected $email;
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function setName() {
+		$this->name = $name;
+		return $this;
+	}
+
+	public function getEmail() {
+		return $this->email();
+	}
+
+	public function setEmail($email) {
+		$this->email = $email;
+		return $this;
+	}
+}
