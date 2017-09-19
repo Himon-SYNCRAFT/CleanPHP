da082912c7e53ff0f2dfc9f1722554bf2176395d
Daniel Zawlocki <d.zawlocki@eltrox.pl>
2017-09-19 15:12:43 +0200

    database setup

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,59 @@
  * file.
  */
 
-return array(
-    // ...
-);
+
+use CleanPhp\Invoicer\Domain\Entity\Customer;
+use CleanPhp\Invoicer\Persistence\Zend\DataTable\CustomerTable;
+use CleanPhp\Invoicer\Persistence\Zend\TableGateway\TableGatewayFactory;
+use Zend\Stdlib\Hydrator\ClassMethods;
+
+
+return [
+	'service_manager' => [
+		'factories' => [
+			'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+			'CustomerTable' => function($sm) {
+				$factory = new TableGatewayFactory();
+				$hydrator = new ClassMethods();
+
+				return new CustomerTable(
+					$factory->createGateway(
+						$sm->get('Zend\Db\Adapter\Adapter'),
+						$hydrator,
+						new Customer(),
+						'customers'
+					),
+					$hydrator
+				);
+			},
+			'InvoiceTable' => function($sm) {
+				$factory = new TableGatewayFactory();
+				$hydrator = new ClassMethods();
+
+				return new InvoiceTable(
+					$factory->createGateway(
+						$sm->get('Zend\Db\Adapter\Adapter'),
+						$hydrator,
+						new Invoice(),
+						'invoices'
+					),
+					$hydrator
+				);
+			},
+			'OrderTable' => function($sm) {
+				$factory = new TableGatewayFactory();
+				$hydrator = new ClassMethods();
+
+				return new OrderTable(
+					$factory->createGateway(
+						$sm->get('Zend\Db\Adapter\Adapter'),
+						$hydrator,
+						new Order(),
+						'ordrers'
+					),
+					$hydrator
+				);
+			},
+		]
+	]
+];
